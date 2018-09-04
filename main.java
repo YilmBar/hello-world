4f49cbe1854e5009f916fccee8f299eb1ee08672
YilmBar <baran.yilmaz@daimler.com>
2018-09-04 11:43:57 +0200

    update 1 main.java

--- main.java
+++ main.java
@@ -1,8 +1,21 @@
 public static void main (String[] args){
 
 
+	// Hello World Programm :)
+
 	System.out.println("Hello World");
 
 
+
+}
+
+	// Erste Methode, addieren zweier geraden Zahlen
+
+public static Int (int x, int y){
+
+	int z = x + y;
+
+
+	return z
 
 }
